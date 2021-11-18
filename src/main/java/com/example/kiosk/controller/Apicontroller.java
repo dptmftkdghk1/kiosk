055b50a5467e4d7c274fb13070730dad6d40a6c6
seojiwon99 <83777261+seojiwon99@users.noreply.github.com>
2021-11-18 10:44:11 +0900

    1

--- src/main/java/com/example/kiosk/controller/Apicontroller.java
+++ src/main/java/com/example/kiosk/controller/Apicontroller.java
@@ -1,10 +1,23 @@
 package com.example.kiosk.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.kiosk.Repository.MenuRepository;
+import com.example.kiosk.data.Menu;
 
 @RestController
 @RequestMapping("/api")
 public class Apicontroller {
-
+	@Autowired
+	private MenuRepository menuRepository;
+	
+	@GetMapping("/menus")
+	public List<Menu> getMenus(){
+		return menuRepository.findAll();
+	}
 }
--- src/main/java/com/example/kiosk/controller/Webcontroller.java
+++ src/main/java/com/example/kiosk/controller/Webcontroller.java
@@ -2,6 +2,7 @@ package com.example.kiosk.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.kiosk.Repository.MenuRepository;
@@ -23,6 +24,11 @@ public class Webcontroller {
 		@GetMapping("manage")	
 		public String menumanagemethod() {	
 			return "menumanage";
+		}
+		@GetMapping("/menus")
+		public String menus(Model model) {
+			model.addAttribute("menus",menuRepository.findAll());
+			return "menus";
 		}
 		@GetMapping("basket")
 		public String basketmethod(){
